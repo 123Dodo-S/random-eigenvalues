597d6618e5653f82de2b5e1de56c1ad1d5c248bc
SenYanHo <80093853+123Dodo-S@users.noreply.github.com>
2022-05-16 22:00:46 +0800

    Now plots distribution for random uniform matrix

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+# Declare Constants
+NUM = 100000
+BINS = 50
+
+# Manage Imports
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Fix seed for debugging purposes
 # np.random.seed(0)
 
-matrix = np.random.rand(10, 10)
+largeEigens = []
 
-eigenvalues, eigenvectors = np.linalg.eig(matrix)
+for i in range(NUM):
+    matrix = (np.random.rand(10, 10) - 0.5) * 100
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    largeEigens.append(np.min(eigenvalues))
 
-print(np.max(eigenvalues))
+# Creates a histogram
+plt.hist(largeEigens, bins=BINS)
+  
+plt.xlabel('Max Eigenvalues')
+plt.ylabel('Frequency')
+
+plt.title('Largest Eigenvalues')
+
+# function to show the plot
+plt.show()
